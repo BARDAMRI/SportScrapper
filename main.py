afddf3b9c843f6c254ff6875dbce0b5cd67c402c
BARDAMRI <bardamri1702@gmail.com>
2024-06-02 19:53:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+from playManager import playManager
+
+config_file = 'config.json'
+config_path = os.path.join(os.path.dirname(__file__), config_file)
+
+try:
+    with open(config_path, 'r') as file:
+        config = json.load(file)
+        print("Configurations loaded successfully:", config)
+        # You can use the configuration as needed here
+except FileNotFoundError:
+    print(f"Error: The configuration file '{config_file}' was not found.")
+except json.JSONDecodeError:
+    print(f"Error: The configuration file '{config_file}' contains invalid JSON.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+if config and config['url'] and config['username'] and config['password']:
+    # Login and invest
+    manager = playManager()
+    manager.login(config['url'], config['username'], config['password'])
+    manager.play()
---
+++ playManager.py
@@ -0,0 +1,16 @@
+class playManager():
+    def __init__(self):
+        self.url = ""
+        self.username = ""
+        self.password = ""
+
+    def login(self, url, username, password):
+        print('Implementation needed on playManager -> login')
+        self.url = url
+        self.username = username
+        self.password = password
+
+    def play(self):
+        print('Implementation needed on playManager -> play')
+        continuePlay = True
+        
